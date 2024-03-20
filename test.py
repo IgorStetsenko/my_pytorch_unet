ad92ace3178c60b4155292bf205f05ab204518e7
IgorStetsenko <i.a.stetsenko@gmail.com>
2024-03-20 22:15:42 +0300

    add f14

--- test.py
+++ test.py
@@ -39,9 +39,11 @@ def test_function11():
 def test_function12():
     pass
 
-def test_function1():
+def test_function13():
     pass
 
+def test_function1():
+    pass
 if __name__ == "__main__":
     test_function()
 	test_function5()
